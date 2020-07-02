ddbac64f995713cbc922ba042fa2f4cb87f2ab70
Roni Lindholm <lroni@helka.dog>
2020-07-02 15:37:22 +0000

    Add docstring, simplify code and fix timezone issues

--- backend/app/measurements/views.py
+++ backend/app/measurements/views.py
@@ -1,18 +1,31 @@
 import json
 from datetime import timedelta
-
+import random
+import pytz
 from django.db.models import Avg, Sum
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
+from django.utils.timezone import get_current_timezone
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Measurement
-from .utils import get_minutes, get_night_low_measurements, save_measurement, authorized
+from .utils import COLORS, get_minutes, get_night_low_measurements, save_measurement, authorized, get_measurements_from_now
 
 
 @csrf_exempt
 def create_measurement(request):
+    """
+    Create a measurement record to database. Endpoint excepts to receive a list of
+    measurements.
+
+    If server configuration includes API_TOKEN environment variable, it must be included in
+    authorazion header. See more about authentication from documentation.
+
+    Required fields for measurement object are:
+        value (float): Value to record, e.g. temperature
+        sensor (string): A identifier for sensor where the value is from 
+    """
     if not authorized(request):
         return HttpResponse("Unauthorized", status=401)
     data = json.loads(request.body)
@@ -30,27 +43,28 @@ def index(request):
     now = timezone.now()
 
     measurements_of_night = get_night_low_measurements()
-    latest_measurements = Measurement.objects \
-        .filter(timestamp__gt=now - timedelta(days=1)) \
+    latest_measurements = get_measurements_from_now(1) \
         .order_by('sensor', '-timestamp').distinct('sensor')
 
-    averages = Measurement.objects \
-        .filter(timestamp__gt=now - timedelta(hours=1)) \
+    averages = get_measurements_from_now(1) \
         .values('sensor').annotate(avg=Avg('value'), sum=Sum('value'))
     
-    stats_by_sensor = {
-        sensor["sensor"]: {
-            "avg": sensor["avg"],
-            "sum": sensor["sum"]
-        } for sensor in averages
-    }
-
+    stats_by_sensor = {}
+    for measurement in averages:
+        last_hours = get_measurements_from_now(1, sensor=measurement["sensor"])
+        stats_by_sensor[measurement["sensor"]] = {
+            "avg": measurement["avg"],
+            "sum": (last_hours.latest("timestamp").value - 
+                last_hours.earliest("timestamp").value)
+        }
+    
     context = {
         "latest_measurements": [{
             "sensor": measurement.sensor,
             "value": measurement.value,
             "type": "temperature",
-            "timestamp": measurement.timestamp.strftime("%H:%M %d.%m."),
+            "timestamp": measurement.timestamp.astimezone(get_current_timezone()) \
+                .strftime("%H:%M %d.%m."),
             "mins_ago": get_minutes(now - measurement.timestamp),
             "hour_avg": round(stats_by_sensor[measurement.sensor]["avg"], 2),
             "hour_delta": round(stats_by_sensor[measurement.sensor]["sum"], 2)
@@ -58,7 +72,8 @@ def index(request):
         "night_low": [{
             "sensor": measurement.sensor,
             "value": measurement.value,
-            "timestamp": measurement.timestamp.strftime("%H:%M %d.%m."),
+            "timestamp": measurement.timestamp.astimezone(get_current_timezone()) \
+                .strftime("%H:%M %d.%m."),
         } for measurement in measurements_of_night]
     }
 
@@ -71,26 +86,31 @@ def graphs(request):
     except ValueError:
         return render(request, 'measurements/charts.html')
 
-    limit_timestamp = timezone.now() - timedelta(hours=limit_hours)
-    measurements_temperature = Measurement.objects \
-        .filter(timestamp__gt=limit_timestamp, type=Measurement.TYPE_TEMPERATURE)
+    measurements_temperature = get_measurements_from_now(
+        limit_hours, measurement_type=Measurement.TYPE_TEMPERATURE
+    )
 
     measurements_by_sensor = {}
     all_labels = set()
     for measurement in measurements_temperature:
         items = measurements_by_sensor.get(str(measurement.sensor), [])
-        formated_timestamp = measurement.timestamp.strftime("%H:%M %d.%m.")
+        formated_timestamp = measurement.timestamp.timestamp()
         items.append({
-            "x": formated_timestamp,
+            "x": measurement.timestamp.astimezone(get_current_timezone()) \
+                .strftime("%H:%M %d.%m."),
             "y": measurement.value
         })
-        all_labels.add(formated_timestamp)
+        all_labels.add(measurement.timestamp)
         measurements_by_sensor.update({str(measurement.sensor): items})
 
+
     context = {
-        'datasets': [{"label": f'Sensor {key}', "data": values}
+        'datasets': [{"label": f'Sensor {key}', "data": values, "fill": "false",
+                      "borderColor": random.choice(COLORS)}
                      for key, values in measurements_by_sensor.items()],
-        "labels": list(all_labels)
+        "labels": [i.astimezone(get_current_timezone()).strftime("%H:%M %d.%m.")
+                   for i in sorted(list(all_labels))],
+        "limit": limit_hours
     }
 
     return render(request, 'measurements/charts.html', context)
